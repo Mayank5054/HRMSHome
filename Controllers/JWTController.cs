5d96318567a7032abf126ea88784f7e55a2e2e6f
Mayank5054 <mayanksheladiya49@gmail.com>
2024-05-21 11:10:31 +0530

    Added : JWT Controllers

---
+++ Controllers/JWTController.cs
@@ -0,0 +1,26 @@
+﻿using HRMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HRMS.Controllers
+{
+    public class JWTController : Controller
+    {
+        // GET: JWT
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginUser _loginUser)
+        {
+            Console.WriteLine(_loginUser);
+            var json = 2;
+            return Json(new { json });
+        }
+    }
+}
