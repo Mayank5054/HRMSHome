7ebbdbf0d00c2e1a2dfbc5b517eb690e4bc49cdf
Mayank5054 <mayanksheladiya49@gmail.com>
2024-05-24 18:57:38 +0530

    gfhfghfg

---
+++ Controllers/FunController.cs
@@ -0,0 +1,27 @@
+﻿using HRMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HRMS.Controllers
+{
+    public class FunController : Controller
+    {
+        // GET: Fun
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(List<fun> _obj)
+        {
+            foreach(fun i in _obj)
+            {
+                
+            }
+            return View();
+        }
+    }
+}
---
+++ Models/fun.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HRMS.Models
+{
+    public class fun
+    {
+        public string question {get;set;}
+        public string option1 { get; set; }
+        public string option2 { get; set; }
+        public string option3 { get; set; }
+        public string option4 { get; set; }
+        public string answer { get; set; }
+    }
+}
